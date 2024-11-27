fe052ea517e9de0c8899a6d7f4a5d593a73879f9
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-11-27 05:29:10 -0500

    added the Admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,12 +1,10 @@
 
 class Admin::DashboardController < ApplicationController  
-  before_action :authenticate
-
-  private
-
-  def authenticate
-    http_basic_authenticate_with(name:"Jungle", password:"book")
-  end
+  http_basic_authenticate_with(name:"Jungle", password:"book")
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
   end
+  private
+  
 end
